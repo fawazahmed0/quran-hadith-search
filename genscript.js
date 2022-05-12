e0256fe26fb2e27e8a69832155a698df559acfbc
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-05-13 03:03:18 +0530

    add quran support

--- genscript.js
+++ genscript.js
@@ -7,11 +7,15 @@ let quranLinks = ["https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/", "https
 let extensions = [".min.json", ".json"]
 let bigJSON = {}
 
+const chaptersJSON = { 'Al-Faatiha': 'The Opening', 'Al-Baqara': 'The Cow', 'Aal-i-Imraan': 'The Family of Imraan', 'An-Nisaa': 'The Women', 'Al-Maaida': 'The Table', "Al-An'aam": 'The Cattle', "Al-A'raaf": 'The Heights', 'Al-Anfaal': 'The Spoils of War', 'At-Tawba': 'The Repentance', Yunus: 'Jonas', Hud: 'Hud', Yusuf: 'Joseph', "Ar-Ra'd": 'The Thunder', Ibrahim: 'Abraham', 'Al-Hijr': 'The Rock', 'An-Nahl': 'The Bee', 'Al-Israa': 'The Night Journey', 'Al-Kahf': 'The Cave', Maryam: 'Mary', 'Taa-Haa': 'Taa-Haa', 'Al-Anbiyaa': 'The Prophets', 'Al-Hajj': 'The Pilgrimage', 'Al-Muminoon': 'The Believers', 'An-Noor': 'The Light', 'Al-Furqaan': 'The Criterion', "Ash-Shu'araa": 'The Poets', 'An-Naml': 'The Ant', 'Al-Qasas': 'The Stories', 'Al-Ankaboot': 'The Spider', 'Ar-Room': 'The Romans', Luqman: 'Luqman', 'As-Sajda': 'The Prostration', 'Al-Ahzaab': 'The Clans', Saba: 'Sheba', Faatir: 'The Originator', Yaseen: 'Yaseen', 'As-Saaffaat': 'Those drawn up in Ranks', Saad: 'The letter Saad', 'Az-Zumar': 'The Groups', Ghafir: 'The Forgiver', Fussilat: 'Explained in detail', 'Ash-Shura': 'Consultation', 'Az-Zukhruf': 'Ornaments of gold', 'Ad-Dukhaan': 'The Smoke', 'Al-Jaathiya': 'Crouching', 'Al-Ahqaf': 'The Dunes', Muhammad: 'Muhammad', 'Al-Fath': 'The Victory', 'Al-Hujuraat': 'The Inner Apartments', Qaaf: 'The letter Qaaf', 'Adh-Dhaariyat': 'The Winnowing Winds', 'At-Tur': 'The Mount', 'An-Najm': 'The Star', 'Al-Qamar': 'The Moon', 'Ar-Rahmaan': 'The Beneficent', 'Al-Waaqia': 'The Inevitable', 'Al-Hadid': 'The Iron', 'Al-Mujaadila': 'The Pleading Woman', 'Al-Hashr': 'The Exile', 'Al-Mumtahana': 'She that is to be examined', 'As-Saff': 'The Ranks', "Al-Jumu'a": 'Friday', 'Al-Munaafiqoon': 'The Hypocrites', 'At-Taghaabun': 'Mutual Disillusion', 'At-Talaaq': 'Divorce', 'At-Tahrim': 'The Prohibition', 'Al-Mulk': 'The Sovereignty', 'Al-Qalam': 'The Pen', 'Al-Haaqqa': 'The Reality', "Al-Ma'aarij": 'The Ascending Stairways', Nooh: 'Noah', 'Al-Jinn': 'The Jinn', 'Al-Muzzammil': 'The Enshrouded One', 'Al-Muddaththir': 'The Cloaked One', 'Al-Qiyaama': 'The Resurrection', 'Al-Insaan': 'Man', 'Al-Mursalaat': 'The Emissaries', 'An-Naba': 'The Announcement', "An-Naazi'aat": 'Those who drag forth', Abasa: 'He frowned', 'At-Takwir': 'The Overthrowing', 'Al-Infitaar': 'The Cleaving', 'Al-Mutaffifin': 'Defrauding', 'Al-Inshiqaaq': 'The Splitting Open', 'Al-Burooj': 'The Constellations', 'At-Taariq': 'The Morning Star', "Al-A'laa": 'The Most High', 'Al-Ghaashiya': 'The Overwhelming', 'Al-Fajr': 'The Dawn', 'Al-Balad': 'The City', 'Ash-Shams': 'The Sun', 'Al-Lail': 'The Night', 'Ad-Dhuhaa': 'The Morning Hours', 'Ash-Sharh': 'The Consolation', 'At-Tin': 'The Fig', 'Al-Alaq': 'The Clot', 'Al-Qadr': 'The Power, Fate', 'Al-Bayyina': 'The Evidence', 'Az-Zalzala': 'The Earthquake', 'Al-Aadiyaat': 'The Chargers', "Al-Qaari'a": 'The Calamity', 'At-Takaathur': 'Competition', 'Al-Asr': 'The Declining Day, Epoch', 'Al-Humaza': 'The Traducer', 'Al-Fil': 'The Elephant', Quraish: 'Quraysh', "Al-Maa'un": 'Almsgiving', 'Al-Kawthar': 'Abundance', 'Al-Kaafiroon': 'The Disbelievers', 'An-Nasr': 'Divine Support', 'Al-Masad': 'The Palm Fibre', 'Al-Ikhlaas': 'Sincerity', 'Al-Falaq': 'The Dawn', 'An-Naas': 'Mankind' };
+const arabicChapters = Object.keys(chaptersJSON)
+const englishChapters = Object.values(chaptersJSON)
+
 async function test() {
 
     let editionsJSON = await getJSON('editions')
+    let quranEditionsJSON = await getJSON('editions', quranLinks)
     let isocodes = await getJSON('isocodes/iso-codes', quranLinks)
-    let infoJSON = await getJSON('info')
 
     let hadithPath = path.join(__dirname, 'docs','Hadiths')
     let quranPath = path.join(__dirname, 'docs','Quran')
@@ -31,9 +35,9 @@ async function test() {
 
             for (let hadith of hadiths) {
                 let pathToSave = path.join(hadithPath,editionsJSON[bareedition].name,`${capitalize(bareedition)} - ${Math.floor(hadith.hadithnumber)}.mdx`)
-                let dataToSave = getHadithCardElem(hadith, edition, dirval, lang, isocodes)
+                let dataToSave = getHadithCardElem(hadith, dirval, lang, isocodes)
                 // save language if doesn't exists
-                if(Array.isArray(bigJSON[pathToSave]) && !bigJSON[pathToSave].includes(languageHeading))
+                if(pathToSave in bigJSON === false ||  !bigJSON[pathToSave].includes(languageHeading))
                 addToBigJSON(pathToSave, languageHeading )
 
                 addToBigJSON(pathToSave, dataToSave )
@@ -42,6 +46,27 @@ async function test() {
 
         }
         
+    }
+    
+    for (let value of Object.values(quranEditionsJSON).sort((a,b)=>a.language.localeCompare(b.language))) {
+        let data = await getJSON(`editions/${value.name}`, quranLinks)
+        let languageHeading = `## ${value.language}`
+
+        for(let quran of data.quran){
+            let chapterName = `Chapter ${quran.chapter} ${arabicChapters[quran.chapter - 1]}`
+            let pathToSave = path.join(quranPath,chapterName,`${arabicChapters[quran.chapter - 1]} Verse ${quran.verse}.mdx`)
+            let dataToSave =  getQuranCardElem(quran, value.direction, value.language,value.author, isocodes) 
+                            // save language if doesn't exists
+                            if(pathToSave in bigJSON === false || !bigJSON[pathToSave].includes(languageHeading))
+                            addToBigJSON(pathToSave, languageHeading )
+            
+                            addToBigJSON(pathToSave, dataToSave )
+                            
+        }
+
+
+
+
     }
 
     for(let [pathToSave, dataArr] of Object.entries(bigJSON)){
@@ -54,11 +79,11 @@ test()
 
 
 // pass hadith object & get card element with all hadith info in it
-function getHadithCardElem(hadith, editionName, dirval, lang, isocodes) {
+function getHadithCardElem(hadith, dirval, lang, isocodes) {
     let str = ''
     let lowerLang = lang.toLowerCase()
     str += `<div dir="${dirval}" lang="${isocodes[lowerLang].iso1 ? isocodes[lowerLang].iso1 : isocodes[lowerLang].iso2}" style={{fontSize:'larger',backgroundColor:'#f8f9fa',padding:20}}>${(hadith.text).replace(/`/gi,"'")}</div>`
-    str+=`<div style={{backgroundColor:'#f8f9fa',padding:20}}>`
+    str+=`<div style={{backgroundColor:'#f8f9fa',padding:20, marginBottom: 10}}>`
     if (hadith.grades.length > 0) {
         str += `<table>
       <thead>
@@ -98,7 +123,33 @@ function getHadithCardElem(hadith, editionName, dirval, lang, isocodes) {
     }
     str += `</tbody></table></div>`
 
-    return str + '\n'
+    return str
+}
+
+
+// pass hadith object & get card element with all hadith info in it
+function getQuranCardElem(quran, dirval, lang,authorName, isocodes) {
+    let str = ''
+    let lowerLang = lang.toLowerCase()
+    str += `<div dir="${dirval}" lang="${isocodes[lowerLang].iso1 ? isocodes[lowerLang].iso1 : isocodes[lowerLang].iso2}" style={{fontSize:'larger',backgroundColor:'#f8f9fa',padding:20}}>${(quran.text).replace(/`/gi,"'")}</div>`
+    str+=`<div style={{backgroundColor:'#f8f9fa',padding:20, marginBottom: 10}}>`
+
+        str += `<table>
+      <thead>
+        <tr>
+          <th>Details</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+`
+    
+
+        str += `<tr><td>Author</td><td>${authorName}</td></tr>`
+
+        str += `</tbody></table></div>`
+
+    return str
 }
 
 
