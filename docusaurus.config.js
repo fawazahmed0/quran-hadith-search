3bf975781a512952620f10c671b994990be91f8a
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-05-12 08:20:18 +0530

    add search bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,6 +127,21 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+    themes: [
+      // ... Your other themes.
+      [
+        require.resolve("@easyops-cn/docusaurus-search-local"),
+        {
+          // ... Your options.
+          // `hashed` is recommended as long-term-cache of index file is possible.
+          hashed: true,
+          // For Docs using Chinese, The `language` is recommended to set to:
+          // ```
+          // language: ["en", "zh"],
+          // ```
+        },
+      ],
+    ],
 };
 
 module.exports = config;
