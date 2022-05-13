c274a1187f6b8662050e086e2ca6cfd971425d84
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-05-13 12:54:31 +0530

    Revert "set parallelism to 1"
    This reverts commit 4fca83f2e62754150f1432f8721028d68b47f8b0.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,18 +127,6 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
-    plugins: [
-      function customWebpackConfig() {
-        return {
-          name: 'custom-webpack-config',
-          configureWebpack(config, isServer, utils) {
-            return {
-              parallelism:1,
-            };
-          },
-        };
-      },
-    ],
     themes: [
       // ... Your other themes.
       [
