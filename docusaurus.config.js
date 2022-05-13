c0bb486cd14230bd6e270a45b9cb3be0655ac850
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-05-14 04:50:17 +0530

    Revert "try esbuild-loader again to reduce build time"
    This reverts commit 045e8a3ae3972f20b72fd7a306face258ec9f170.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,16 +127,6 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
-    webpack: {
-      jsLoader: (isServer) => ({
-        loader: require.resolve('esbuild-loader'),
-        options: {
-          loader: 'tsx',
-          format: isServer ? 'cjs' : undefined,
-          target: isServer ? 'node12' : 'es2017',
-        },
-      }),
-    },
     themes: [
       // ... Your other themes.
       [
