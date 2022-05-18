1c6b413199512e1b67c5e930abd24501ef75e68e
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-05-18 11:45:49 +0530

    Update copydir.js

--- copydir.js
+++ copydir.js
@@ -2,3 +2,4 @@ const fs = require('fs-extra')
 const path = require('path')
 // copy docs to build folder, so that edit this page can work
 fs.copySync(path.join(__dirname, 'docs'), path.join(__dirname, 'build','docsdata','docs'))
+fs.copySync(path.join(__dirname, 'blog'), path.join(__dirname, 'build','docsdata','blog'))
