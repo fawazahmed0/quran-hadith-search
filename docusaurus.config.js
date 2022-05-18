8d34696c24dfea7a4ceae6eab8c3f37d9f1e39c1
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-05-18 11:44:21 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/fawazahmed0/quran-hadith-search/tree/gh-pages/',
+            'https://github.com/fawazahmed0/quran-hadith-search/tree/gh-pages/docsdata/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/fawazahmed0/quran-hadith-search/tree/gh-pages/',
+            'https://github.com/fawazahmed0/quran-hadith-search/tree/gh-pages/docsdata/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
