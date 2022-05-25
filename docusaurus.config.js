b02a48569506e084e3e58578005e74a5cdd2d9b7
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-05-26 01:05:59 +0530

    Revert "try lazy compilation ram usage"
    This reverts commit d6f162d54b985d914944513320d144d74786a9f0.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -109,18 +109,6 @@ const config = {
       },
     }),
     plugins: [
-      function customWebpackConfig() {
-        return {
-          name: 'custom-webpack-config',
-          configureWebpack(config, isServer, utils) {
-            return {
-              experiments: {
-                lazyCompilation: true,
-              },
-            };
-          },
-        };
-      },
       [
         '@docusaurus/plugin-pwa',
         {
