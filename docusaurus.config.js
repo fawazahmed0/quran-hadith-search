d3f405e13762940ec54d951e746d29d712b24ce4
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-05-29 22:44:47 +0530

    Add lunr

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -126,6 +126,21 @@ const config = {
         },
       }),
     },
+    themes: [
+      // ... Your other themes.
+      [
+        require.resolve("@easyops-cn/docusaurus-search-local"),
+        {
+          // ... Your options.
+          // `hashed` is recommended as long-term-cache of index file is possible.
+          hashed: true,
+          // For Docs using Chinese, The `language` is recommended to set to:
+          // ```
+           language: ["en","ar", "da", "de","zh", "es", "fi", "fr", "hu", "it", "jp", "nl", "no", "pt", "ro", "ru","sv","tr"],
+          // ```
+        },
+      ],
+    ],
     plugins: [
       [
         '@docusaurus/plugin-pwa',
