698318f397b39ad83559f83d67dab87697596982
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-06-02 04:11:28 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -126,20 +126,6 @@ const config = {
         },
       }),
     },
-    themes: [
-      // ... Your other themes.
-      [
-        require.resolve("@easyops-cn/docusaurus-search-local"),
-        {
-          // ... Your options.
-          // `hashed` is recommended as long-term-cache of index file is possible.
-          hashed: true,
-          // For Docs using Chinese, The `language` is recommended to set to:
-          // ```
-          // ```
-        },
-      ],
-    ],
     plugins: [
       [
         '@docusaurus/plugin-pwa',
