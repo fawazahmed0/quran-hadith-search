609a647b9c3b5156b0cb42664d545010c028ba28
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-06-23 00:01:50 +0530

    run build again

--- copydir.js
+++ copydir.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra')
 const path = require('path')
-if(process.env.TEST_BUILD)
+if(process.env.TEST_BUILD) 
 fs.copySync(path.join(__dirname, 'testdocs'), path.join(__dirname, 'docs'))
 // copy docs to build folder, so that edit this page can work
 fs.copySync(path.join(__dirname, 'docs'), path.join(__dirname, 'build','docsdata','docs'))
